30d0248ad56eb58ccc9c2db022f75e82c85903bd
rpgyumyum <CarterMoony@proton.me>
2023-10-12 09:25:46 +0200

    fixed paths

--- todo/todo-source.cpp
+++ todo/todo-source.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <fstream>
-
 #include <string>
 #include <cstring>
-
 #include <algorithm>
 #include <iterator>
 
-
+#define TODO "/home/Lukas/Documents/todo.txt" 
+#define TEMP "/home/Lukas/Documents/todo.txt" 
 
 
 void displayList()
 {
   std::string line;
-  std::fstream list ("/home/Lukas/Documents/todo.txt");
+  std::fstream list (TODO);
 
   if (list.is_open())
   {
-
-
     std::cout << '\n';
-
     int linesInFile{0};
 
     while (std::getline(list, line))
@@ -38,11 +34,7 @@ void displayList()
       std::cout << index++ << " - " << line << '\n';
     }
 
-
-
-
     std::cout << '\n';
-
     list.close();
   }
   else
@@ -57,7 +49,7 @@ void displayList()
 
 void addListItem(std::string newItem)
 {
-  std::ofstream list("/home/Lukas/Documents/todo.txt", std::ios::app);
+  std::ofstream list(TODO, std::ios::app);
 
   if (list.is_open())
   {
@@ -79,8 +71,8 @@ void changeListItem(std::string string_index, std::string newLine)
 {
   int index = atoi(string_index.c_str());
 
-  std::fstream list("/home/Lukas/Documents/todo.txt");
-  std::ofstream temp("temp.txt");
+  std::fstream list(TODO);
+  std::ofstream temp(TEMP);
 
   if (list.is_open() && temp.is_open())
   {
@@ -108,8 +100,8 @@ void changeListItem(std::string string_index, std::string newLine)
     list.close();
     temp.close();
 
-    std::remove("/home/Lukas/Documents/todo.txt");
-    std::rename("temp.txt", "/home/Lukas/Documents/todo.txt");
+    std::remove("TODO");
+    std::rename("TEMP", "TODO");
 
     if (!lineFound)
     {
@@ -141,16 +133,16 @@ int main(int argc, char const *argv[])
   }
   else if (argc == 3)
   {
-    if(!strcmp(argv[1], "-add")) //ADD
+    if(!strcmp(argv[1], "--add") || !strcmp(argv[1], "-a")) //ADD
     {
       addListItem(argv[2]);
     }
-    else if (!strcmp(argv[1], "-done")) //DONE
+    else if (!strcmp(argv[1], "--done") || !strcmp(argv[1], "-d")) //DONE
     {
       changeListItem(argv[2], "");
     }
   }
-  else if (argc == 4 && !strcmp(argv[1], "-mod")) //CHANGE A LINE
+  else if (argc == 4 && (!strcmp(argv[1], "--mod") || !strcmp(argv[1], "-m"))) //CHANGE A LINE
   {
     changeListItem(argv[2], argv[3]);
   }
